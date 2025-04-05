c5d1a545a568030b0e27cacc6a772be73f482a49
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-04-05 14:55:48 +0800

    [add] sections

---
+++ frontend/src/sections/Contact.jsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@chakra-ui/react';
+function Contact() {
+  return (
+    // move the id to the h2 of this
+    <Container id={'contact'} maxW={'container.lg'} h={'100vh'} bg={'green'}>
+      <Text>Contact</Text>
+    </Container>
+  );
+}
+
+export default Contact;
