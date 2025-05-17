d2e47212a1b963561ef67977061e48f89dc15208
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-05-18 02:48:16 +0800

    [upd] Home.jsx

--- frontend/src/sections/Home.jsx
+++ frontend/src/sections/Home.jsx
@@ -1,12 +1,26 @@
-import { Container } from '@chakra-ui/react';
-import About from './About';
-// import Projects from './Projects';
-// import Contact from './Contact';
+import {
+  Container,
+  VStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { Navbar } from '../components/Navbar';
+import About from './About/About';
+import Projects from './Projects/Projects';
+import Contact from './Contact/Contact';
 
 function Home() {
   return (
-    <Container maxW={'container.xxl'}>
-      <About />
+    <Container
+      maxW="container.xxl"
+      minW="container.sm"
+      bg={useColorModeValue('#e5ded2', '#1A212D')}
+    >
+      <Navbar />
+      <VStack>
+        <About />
+        {/* <Projects /> */}
+        {/* <Contact /> */}
+      </VStack>
     </Container>
   );
 }
