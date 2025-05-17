2745ac1aa60692e5f179ba350707f20f0a7e08bf
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-05-18 02:44:46 +0800

    [upd] remove dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import { Container, useColorModeValue } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
 import Home from './sections/Home';
 import './app.css';
 
 function App() {
   return (
-    <Container maxW={'container.xxl'} minH="100vh" bg={useColorModeValue('#e5ded2', '#1A212D')}>
-      <Navbar />
+    <div>
       <Routes>
         <Route path={'/'} element={<Home />}></Route>
       </Routes>
-    </Container>
+    </div>
   );
 }
 
