ac37595df5d57066cd38641db308846b9cf203fe
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-05-19 22:58:33 +0800

    [upd] about.jsx

--- frontend/src/sections/About/About.jsx
+++ frontend/src/sections/About/About.jsx
@@ -1,29 +1,27 @@
 import { Container, Grid, useBreakpointValue } from '@chakra-ui/react';
 import Namecard from './Namecard';
 import Techstacks from './Techstacks';
-import Contribution from './Contribution';
 
 function About() {
   return (
     <Container id="about" maxW="container.lg">
       <Grid
         templateRows={useBreakpointValue({
-          base: 'repeat(6, 1fr)',
-          sm: 'repeat(5, 1fr)',
-          md: 'repeat(3, 1fr)',
-          lg: 'repeat(5, 1fr)',
+          base: 'repeat(1, 1fr)',
+          sm: 'repeat(1, 1fr)',
+          md: 'repeat(1, 1fr)',
+          lg: 'repeat(2, 1fr)',
         })}
         templateColumns={useBreakpointValue({
           base: '1fr',
           sm: '1fr',
-          md: '1fr',
+          md: 'repeat(2, 1fr)',
           lg: 'repeat(5, 1fr)',
         })}
         gap={4}
       >
         <Namecard />
         <Techstacks />
-        <Contribution />
       </Grid>
     </Container>
   );
