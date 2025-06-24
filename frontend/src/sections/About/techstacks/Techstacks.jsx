baad8cf0530161b6c9328e2acc153043fea5ed7f
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-06-24 22:54:52 +0800

    [upd] rename and refactor

--- frontend/src/sections/About/techstacks/Techstacks.jsx
+++ frontend/src/sections/About/techstacks/Techstacks.jsx
@@ -2,17 +2,18 @@ import {
   GridItem,
   VStack,
   SimpleGrid,
-  Text,
+  Heading,
   Tooltip,
   Box,
   Image,
   useBreakpointValue,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { useState, useEffect } from 'react';
-import { techstackImgMap } from '@/assets/techstack';
-import techstackData from '@/assets/data/techstack.json';
-import { colorMap } from '@/assets/color';
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { techstackImgMap } from "@/assets/techstack";
+import techstackData from "@/assets/data/techstack.json";
+import { colorMap } from "@/assets/color";
+import "./index.css";
 
 function Techstacks() {
   const [isAnimated, setIsAnimated] = useState(true);
@@ -33,31 +34,23 @@ function Techstacks() {
       className="block"
       rowSpan={useBreakpointValue({
         base: 1,
-        sm: 1,
-        md: 1,
         lg: 2,
       })}
       colSpan={useBreakpointValue({
         base: 1,
-        sm: 1,
-        md: 1,
         lg: 2,
       })}
       borderRadius="xl"
-      bg={useColorModeValue(
-        'rgba(255, 255, 255, 0.35)',
-        'rgba(255, 255, 255, 0.05)'
-      )}
-      backdropFilter="blur(12px)"
+      bg={useColorModeValue(colorMap.light.cardBg, colorMap.dark.cardBg)}
       border={`1px solid ${useColorModeValue(
-        'rgba(0, 0, 0, 1)',
-        'rgba(255, 255, 255, 0.1)'
+        colorMap.light.cardBorder,
+        colorMap.dark.cardBorder
       )}`}
     >
       <VStack mx="auto" p={5} py={4}>
-        <Text as="h2" className="lato" fontSize={22}>
+        <Heading as="h2" className="lato" size="md">
           Tech Stack
-        </Text>
+        </Heading>
         <SimpleGrid
           columns={{ base: 3, sm: 5, md: 3, lg: 4 }}
           gap={4}
@@ -67,23 +60,21 @@ function Techstacks() {
           {techstacks.map(({ src, alt }, idx) => (
             <Tooltip key={idx} label={alt} placement="top" hasArrow>
               <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
+                className={`techstack ${isAnimated ? "moveup" : ""}`}
                 boxSize="20"
-                cursor="pointer"
-                transition="transform 0.3s ease-in-out"
-                _hover={{ transform: 'translateY(-10px)' }}
-                bg="rgba(255, 255, 255, 0.1)"
-                p={2}
+                _hover={{ transform: "translateY(-10px)" }}
+                bg={useColorModeValue(
+                  colorMap.light.buttonBg,
+                  colorMap.dark.buttonBg
+                )}
                 borderRadius="md"
-                className={isAnimated ? 'moveup' : ''}
                 style={{ animationDelay: `${idx}00ms` }}
+                transition="transform 0.3s ease-in-out"
               >
                 <Image
                   src={src}
                   alt={alt}
-                  boxSize={{ base: '70%' }}
+                  boxSize={{ base: "70%" }}
                   objectFit="contain"
                 />
               </Box>
