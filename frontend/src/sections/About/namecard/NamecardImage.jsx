c015ba5a3f7f8545158b1896ab28886b8bbab335
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-06-24 22:55:23 +0800

    [upd] rename and change directory

---
+++ frontend/src/sections/About/namecard/NamecardImage.jsx
@@ -0,0 +1,19 @@
+import { Box, Image } from '@chakra-ui/react';
+import ProfilePicture from '@/assets/profile-picture.jpg';
+
+function NamecardImage() {
+  return (
+    <Box className="profile-pic-container" cursor="pointer">
+      <Image
+        src={ProfilePicture}
+        alt="profile-picture"
+        boxSize={{ base: '150px' }}
+        maxWidth={150}
+        borderRadius="full"
+        objectFit="cover"
+      />
+    </Box>
+  );
+}
+
+export default NamecardImage;
