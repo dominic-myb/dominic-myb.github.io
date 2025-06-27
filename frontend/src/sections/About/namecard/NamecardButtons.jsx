f45e5a722f26aeab54d3d0120fa83c4856666bb6
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-06-27 23:20:38 +0800

    [upd] responsive button sizes

--- frontend/src/sections/About/namecard/NamecardButtons.jsx
+++ frontend/src/sections/About/namecard/NamecardButtons.jsx
@@ -1,14 +1,14 @@
-import { HStack, Button, Text } from '@chakra-ui/react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { SiLeetcode } from 'react-icons/si';
-import { IoMdDownload } from 'react-icons/io';
+import { HStack, Button, Text, useBreakpointValue } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { IoMdDownload } from "react-icons/io";
 
 function DownloadPDFButton() {
   const handleDownload = async () => {
-    const pdf = await import('@/assets/downloads/Dominic-Esguerra-CV.pdf');
-    const link = document.createElement('a');
+    const pdf = await import("@/assets/downloads/Dominic-Esguerra-CV.pdf");
+    const link = document.createElement("a");
     link.href = pdf.default;
-    link.download = 'Dominic-Esguerra-CV.pdf';
+    link.download = "Dominic-Esguerra-CV.pdf";
     link.click();
   };
 
@@ -22,21 +22,21 @@ function DownloadPDFButton() {
 
 function SocialLink({ link, Component }) {
   return (
-    <Button onClick={() => window.open(link, '_blank')}>
-      <Component size="20" />
+    <Button onClick={() => window.open(link, "_blank")}>
+      <Component size={useBreakpointValue({ base: 15, md: 20 })} />
     </Button>
   );
 }
 
 function NamecardButtons() {
   const socialLinks = [
-    { link: 'https://github.com/dominic-myb', component: FaGithub },
+    { link: "https://github.com/dominic-myb", component: FaGithub },
     {
-      link: 'https://leetcode.com/desguerra246/',
+      link: "https://leetcode.com/desguerra246/",
       component: SiLeetcode,
     },
     {
-      link: 'https://www.linkedin.com/in/dominic-esguerra/',
+      link: "https://www.linkedin.com/in/dominic-esguerra/",
       component: FaLinkedin,
     },
   ];
