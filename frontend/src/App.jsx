4f5322ced28ad6e8c05c888469ad5752cf1a615e
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-27 19:10:42 +0800

    [upd] App.jsx added new imports css files and update the import for Home.jsx from sections to pages directory, update the code formatting.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './sections/Home';
-import './app.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "@/assets/styles/global.css";
+import "@/assets/styles/variables.css";
+import "@/assets/styles/responsive.css";
+import "@/assets/styles/animations.css";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Routes>
-        <Route path={'/'} element={<Home />}></Route>
+        <Route path={"/"} element={<Home />}></Route>
       </Routes>
     </div>
   );
 }
-
-export default App;
