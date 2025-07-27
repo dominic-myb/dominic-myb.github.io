e7d76ad5425ea095bfca4ea424f4b351c7589c7e
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-27 19:12:29 +0800

    [upd] combined colorMap and aboutGridSize in contants file

--- frontend/src/assets/data/constants.js
+++ frontend/src/assets/data/constants.js
@@ -1,4 +1,4 @@
-export const colorMap = {
+const colorMap = {
   light: {
     logo: "linear-gradient(to-r, #ff4f64, #6936c2)",
     font: "gray.100",
@@ -18,3 +18,14 @@ export const colorMap = {
     line: "linear(to-b, #00b09b, #96c93d)",
   },
 };
+
+// [1] Namecard
+// [2] Techstack
+// [3] Background
+const aboutGridSize = [
+  { rowSpan: { base: 1, md: 2, lg: 2 }, colSpan: { base: 1, md: 2, lg: 3 } },
+  { rowSpan: { base: 1, md: 2, lg: 2 }, colSpan: { base: 1, md: 2, lg: 2 } },
+  { rowSpan: { base: 3, md: 5, lg: 5 }, colSpan: { base: 1, md: 4, lg: 5 } },
+];
+
+export { colorMap, aboutGridSize };
