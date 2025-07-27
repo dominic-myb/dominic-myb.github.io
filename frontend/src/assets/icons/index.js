3fe0630f2bf5ea12fd8e5b4af9c6e4357f62034a
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-27 19:18:25 +0800

    [add] iconMap

---
+++ frontend/src/assets/icons/index.js
@@ -0,0 +1,36 @@
+import html5 from "./html-5.svg";
+import css3 from "./css-3.svg";
+import javascript from "./javascript.svg";
+import react from "./react.svg";
+import nodejs from "./nodejs-icon.svg";
+import mongodb from "./mongodb-icon.svg";
+import mysql from "./mysql.svg";
+import php from "./php.svg";
+import cpp from "./c-plusplus.svg";
+import jquery from "./jquery.svg";
+import figma from "./figma.svg";
+import godot from "./godot.svg";
+import bootstrap from "./bootstrap.svg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+const iconMap = {
+  html5,
+  css3,
+  javascript,
+  react,
+  nodejs,
+  mongodb,
+  mysql,
+  php,
+  cpp,
+  jquery,
+  figma,
+  godot,
+  bootstrap,
+  FaGithub,
+  FaLinkedin,
+  SiLeetcode
+};
+
+export { iconMap };
