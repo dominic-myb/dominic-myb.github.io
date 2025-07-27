bf18658811a0355a4b49e132e2379c4161ba96b2
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-27 19:28:05 +0800

    [add] downloads.js logic function

---
+++ frontend/src/utils/downloads.js
@@ -0,0 +1,14 @@
+import resume from "@/assets/pdf/Dominic-Esguerra-CV.pdf";
+
+const handleDownload = async (filename = "file.pdf") => {
+  try {
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = filename;
+    link.click();
+  } catch (error) {
+    console.error(`Error downloading pdf: ${error}`);
+  }
+};
+
+export { handleDownload };
