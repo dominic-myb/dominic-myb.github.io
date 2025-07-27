e280e4b55cc71cf18359fce9f76c62b8f80ad0f2
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-27 19:29:21 +0800

    [add] Main Layout of my portfolio website

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Container, VStack, useColorModeValue } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
+import { colorMap } from "@/assets/data/constants";
+
+export default function MainLayout({ children }) {
+  return (
+    <Container
+      maxW="container.xxl"
+      bg={useColorModeValue(
+        colorMap.light.background,
+        colorMap.dark.background
+      )}
+    >
+      <Navbar />
+      <VStack as="main" gap={4}>
+        {children}
+      </VStack>
+    </Container>
+  );
+}
