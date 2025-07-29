1039fbe95496e8a845dc2121f51355282a0c12ea
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-29 22:09:36 +0800

    [add] assemble contact section

---
+++ frontend/src/sections/Contact.jsx
@@ -0,0 +1,9 @@
+import ContactLayout from "@/layouts/ContactLayout";
+import ContactForm from "@/components/ContactForm";
+export default function Contact() {
+  return (
+    <ContactLayout>
+      <ContactForm />
+    </ContactLayout>
+  );
+}
