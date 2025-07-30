fb4a5192497cb98404fcf7bf54b69b450c81b132
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-30 21:45:50 +0800

    [upd] update handledownload

--- frontend/src/utils/downloads.js
+++ frontend/src/utils/downloads.js
@@ -1,14 +1,28 @@
-const resume = "/public/assets/pdf/Dominic-Esguerra-CV.pdf";
-
-const handleDownload = async (filename = "file.pdf") => {
+/**
+ * Downloads a file from the specified URL and saves it with the given filename.
+ *
+ * @param {Object} options
+ * @param {string} options.url - The URL of the file to download.
+ * @param {string} [options.filename="file.pdf"] - The desired name for the downloaded file.
+ */
+async function handleDownload({ url, filename }) {
   try {
+    if (!url || typeof url !== "string") {
+      console.error("Invalid URL provided for download.");
+      return;
+    }
+    const finalFilename = filename || url.split("/").pop() || "file.pdf";
     const link = document.createElement("a");
-    link.href = resume;
-    link.download = filename;
+
+    link.href = url;
+    link.download = finalFilename;
+
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
   } catch (error) {
     console.error(`Error downloading pdf: ${error}`);
   }
-};
+}
 
 export { handleDownload };
