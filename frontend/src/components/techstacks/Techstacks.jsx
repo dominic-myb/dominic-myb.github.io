e209e465f5872f750a3311d56cebb98e77ea691f
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-07-31 13:13:31 +0800

    [upd] techstack card

--- frontend/src/components/techstacks/Techstacks.jsx
+++ frontend/src/components/techstacks/Techstacks.jsx
@@ -1,16 +1,10 @@
 import {
-  Flex,
   VStack,
   Heading,
-  Image,
   SimpleGrid,
-  Tooltip,
-  useColorModeValue,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { iconMap } from "@/assets/icons";
-import { colorMap } from "@/assets/data/constants";
-import techstackData from "@/assets/data/techstack.json";
+import TechstackIcons from "./TechstackIcons";
 
 export default function Techstacks() {
   const [isAnimated, setIsAnimated] = useState(true);
@@ -21,31 +15,37 @@ export default function Techstacks() {
     return () => clearInterval(interval);
   }, []);
 
-  const techstacks = techstackData.map(({ key, alt }) => ({
-    src: iconMap[key],
-    alt,
-  }));
+  const iconsPath = "/assets/icons/";
+  const toolNames = [
+    "html5",
+    "css3",
+    "javascript",
+    "react",
+    "nodejs",
+    "mongodb",
+    "mysql",
+    "php",
+    "cpp",
+    "jquery",
+    "figma",
+    "godot",
+    "bootstrap",
+  ];
+  const filetype = ".svg";
 
   return (
     <VStack mx="auto" p={4}>
       <Heading fontSize={28}>Techstacks</Heading>
       <SimpleGrid columns={4} gap={4} placeItems="center">
-        {techstacks.map(({ src, alt }, idx) => (
-          <Tooltip key={idx} label={alt} placement="top" hasArrow>
-            <Flex
-              className={`techstack ${isAnimated ? "moveup" : ""}`}
-              p={2}
-              boxSize={20}
-              borderRadius="md"
-              bg={useColorModeValue(
-                colorMap.light.buttonBg,
-                colorMap.dark.buttonBg
-              )}
-              style={{ animationDelay: `${idx}00ms` }}
-            >
-              <Image src={src} alt={alt} boxSize="65%" objectFit="contain" />
-            </Flex>
-          </Tooltip>
+        {toolNames.map((img, idx) => (
+          <TechstackIcons
+            key={img}
+            flexClass={`techstack ${isAnimated ? "moveup" : ""}`}
+            imgPath={`${iconsPath}${img}${filetype}`}
+            imgName={img}
+            imgAlt={`${img}${filetype}`}
+            imgIdx={idx}
+          />
         ))}
       </SimpleGrid>
     </VStack>
