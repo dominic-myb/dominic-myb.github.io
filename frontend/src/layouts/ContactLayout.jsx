1878458dc35d2d81cbc31e2a6bb3f6235d63e63a
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-02 02:13:45 +0800

    [fix] navbar to id redirection

--- frontend/src/layouts/ContactLayout.jsx
+++ frontend/src/layouts/ContactLayout.jsx
@@ -22,13 +22,13 @@ export default function ContactLayout({ children }) {
     borderRadius: "xl",
   };
   return (
-    <Container id="contact" maxW="container.lg" h="100vh">
-      <VStack maxW="container.lg" p={4} {...cardStyles}>
-        <VStack mt={10} gap={4}>
+    <Container id="contact" maxW="container.lg" scrollMarginTop="75px">
+      <VStack maxW="container.lg" {...cardStyles}>
+        <VStack mt={8} gap={4}>
           <Heading as="h1" fontSize={36}>
             Get in Touch
           </Heading>
-          <Text textAlign="center" fontSize={18} w="60%">
+          <Text textAlign="center" fontSize={16} w="60%">
             Have a project in mind or just want to say hello? I'd love to hear
             from you. Send me a message and I'll get back to you as soon as
             possible.
