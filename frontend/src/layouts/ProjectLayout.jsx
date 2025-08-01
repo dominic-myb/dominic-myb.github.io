61af56e21107cf71075ffdc8caaa393c35c4134d
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-02 02:14:29 +0800

    [upd] project layout

--- frontend/src/layouts/ProjectLayout.jsx
+++ frontend/src/layouts/ProjectLayout.jsx
@@ -1,25 +1,29 @@
-import { Container, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
+import {
+  Container,
+  VStack,
+  Heading,
+  SimpleGrid,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { colorMap } from "@/assets/data/constants.js";
 
 export default function ProjectLayout({ children }) {
   const _bg = useColorModeValue(colorMap.light.cardBg, colorMap.dark.cardBg);
-  const _border = useColorModeValue(
+  const _border = `1px solid ${useColorModeValue(
     colorMap.light.cardBorder,
     colorMap.dark.cardBorder
-  );
-
-  const containerStyles = {
-    id: "projects",
-    maxW: "container.lg",
-    bg: _bg,
-    border: _border,
-  };
+  )}`;
 
   return (
-    <Container {...containerStyles}>
-      <SimpleGrid columns={2} gap={4}>
-        {children}
-      </SimpleGrid>
+    <Container id="projects" maxW="container.lg" scrollMarginTop="75px">
+      <VStack bg={_bg} border={_border} borderRadius="xl">
+        <Heading mt={6} fontSize={30}>
+          My Projects
+        </Heading>
+        <SimpleGrid p={6} columns={2} gap={6}>
+          {children}
+        </SimpleGrid>
+      </VStack>
     </Container>
   );
 }
