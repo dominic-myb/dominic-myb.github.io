4d4cefbd0c247ba9a91a6e0aaf9d2f8ee845a911
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-02 02:10:48 +0800

    [upd] certificate

---
+++ frontend/src/components/certificates/Certificate.jsx
@@ -0,0 +1,22 @@
+import { VStack, Heading, SimpleGrid } from "@chakra-ui/react";
+import CertificateCard from "./CertificateCard";
+
+export default function Certificate({ cert }) {
+  return (
+    <VStack>
+      <Heading mt={6} fontSize={30}>
+        My Certificates
+      </Heading>
+      <SimpleGrid columns={3} p={6}>
+        {cert.map(({ title, platform, link }, idx) => (
+          <CertificateCard
+            key={idx}
+            title={title}
+            platform={platform}
+            link={link}
+          />
+        ))}
+      </SimpleGrid>
+    </VStack>
+  );
+}
