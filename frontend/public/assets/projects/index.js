f35ad2fb12880ab35fdba7a4a0a18f37a71a3428
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-01 14:01:25 +0800

    [upd] remove project image map and rename project image

--- frontend/public/assets/projects/index.js
+++
@@ -1,9 +0,0 @@
-import DaraThumbnail from "./project-1/thumbnail.jpg";
-// import DaraPreview1 from './project-1/preview-1.jpg';
-// import DaraPreview2 from './project-1/preview-2.jpg';
-
-const ProjectImgMap = {
-  DaraThumbnail,
-};
-
-export { ProjectImgMap };
