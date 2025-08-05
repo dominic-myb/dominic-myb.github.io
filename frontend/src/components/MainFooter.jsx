5a866665f85266419f751a2f4890413609b04350
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-06 04:51:00 +0800

    [upd] colors of footer

--- frontend/src/components/MainFooter.jsx
+++ frontend/src/components/MainFooter.jsx
@@ -1,12 +1,17 @@
 import { Box, Text } from "@chakra-ui/react";
+import { useThemeColor } from "@/assets/colors";
 
 export default function MainFooter() {
+  const [bg, bodyFont, smallFont] = useThemeColor(["bg", "bodyFont", "smallFont"]);
   return (
-    <Box as="footer" bg="gray.100" py={4} textAlign="center">
-      <Text fontSize="sm" color="gray.600">
-         © 2024 - {new Date().getFullYear()} Dominic Esguerra. All rights reserved.
+    <Box as="footer" bg={bg} py={4} textAlign="center">
+      <Text fontSize="sm" color={bodyFont}>
+        © 2024 - {new Date().getFullYear()} Dominic Esguerra. All rights
+        reserved.
       </Text>
-      <Text fontSize="sm" color="gray.600">Made with React + ChakraUI</Text>
+      <Text fontSize="sm" color={smallFont}>
+        Made with React + ChakraUI
+      </Text>
     </Box>
   );
 }
