b7651249f4736a16363ce8bc4b74d00b6dd3c595
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-09 22:51:58 +0800

    [upd] responsive contact form container

--- frontend/src/layouts/ContactLayout.jsx
+++ frontend/src/layouts/ContactLayout.jsx
@@ -1,34 +1,41 @@
-import { Container, VStack, Heading, Text } from "@chakra-ui/react";
+import { Container, VStack, Heading, Text, Box } from "@chakra-ui/react";
 import { useThemeColor } from "@/assets/colors";
 
 export default function ContactLayout({ children }) {
-  const [cardBd, cardBg] = useThemeColor(["cardBd", "cardBg"]);
+  const [cardBd, cardBg, titleFont, bodyFont] = useThemeColor([
+    "cardBd",
+    "cardBg",
+    "titleFont",
+    "bodyFont",
+  ]);
   return (
     <Container id="contact" maxW="container.lg" scrollMarginTop="75px">
-      <VStack
-        maxW="container.lg"
-        bg={cardBg}
-        borderRadius="xl"
-        border={`1px solid ${cardBd}`}
-      >
+      <VStack maxW="container.lg" bg={cardBg} borderRadius="xl">
         <VStack mt={8} gap={4}>
-          <Heading as="h1" fontSize={36}>
+          <Heading as="h1" fontSize="3xl" color={titleFont}>
             Get in Touch
           </Heading>
-          <Text textAlign="center" fontSize={16} w="60%">
+          <Text
+            w="60%"
+            textAlign="center"
+            fontSize={16}
+            color={bodyFont}
+            fontWeight={300}
+          >
             Have a project in mind or just want to say hello? I'd love to hear
             from you. Send me a message and I'll get back to you as soon as
             possible.
           </Text>
         </VStack>
-        <Container
+        <Box
+          w={{ base: "70%", md: "60%" }}
           my={10}
           p={6}
           borderRadius="xl"
           border={`1px solid ${cardBd}`}
         >
           {children}
-        </Container>
+        </Box>
       </VStack>
     </Container>
   );
