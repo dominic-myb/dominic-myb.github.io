3414b6f3026be05b18e45cc3eaa1e829bf93924f
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-09 22:48:33 +0800

    [upd] vite  config

--- frontend/src/assets/colors/index.js
+++ frontend/src/assets/colors/index.js
@@ -2,20 +2,25 @@ import { useColorMode } from "@chakra-ui/react";
 
 const colorMap = {
   logo: {
-    light: "linear-gradient(to-r, #ff4f64, #6936c2)",
+    light: "linear-gradient(to-r, #00b09b, #96c93d)",
     dark: "linear-gradient(to-r, #00b09b, #96c93d)",
   },
-  font: { light: "gray.100", dark: "gray.800" },
-  titleFont: { light: "gray.800", dark: "whiteAlpha.900" },
-  bodyFont: { light: "gray.700", dark: "gray.300" },
-  smallFont: { light: "gray.500", dark: "gray.400" },
-  bg: { light: "rgba(242, 243, 245, 1)", dark: "rgba(14, 16, 20, 1)" },
-  cardBg: { light: "rgba(0, 0, 0, 0.05)", dark: "rgba(255, 255, 255, 0.05)" },
-  cardBd: { light: "rgba(0, 0, 0, 0.125)", dark: "rgba(255, 255, 255, 0.125)" },
-  btnBg: { light: "rgba(0, 0, 0, 0.125)", dark: "rgba(255, 255, 255, 0.125)" },
-  line: {
-    light: "linear-gradient(to-b, #ff4f64, #6936c2)",
-    dark: "linear-gradient(to-b, #00b09b, #96c93d)",
+  invertColor: { light: "hsl(0, 0%, 95%)", dark: "hsl(0, 0%, 5%)" },
+  titleFont: { light: "hsl(246, 76%, 5%)", dark: "hsl(246, 76%, 95%)" },
+  bodyFont: { light: "hsl(0, 0%, 20%)", dark: "hsl(0, 0%, 80%)" },
+  smallFont: { light: "hsl(0, 0%, 40%)", dark: "hsl(0, 0%, 60%)" },
+  bg: { light: "hsl(240, 100%, 100%)", dark: "	hsl(240, 60%, 1%)" },
+  cardBg: { light: "hsl(240, 16%, 94%)", dark: "hsla(0, 0%, 90%, 0.05)" },
+  cardBd: {
+    light: "hsla(0, 0%, 10%, 0.125)",
+    dark: "hsla(0, 0%, 90%, 0.125)",
+  },
+  btnBg: { light: "hsla(0, 0%, 10%, 0.125)", dark: "hsla(0, 0%, 80%, 0.125)" },
+  primary: { light: "hsl(243, 68%, 48%)", dark: "hsl(243, 68%, 52%)" },
+  secondary: { light: "hsl(243, 100%, 93%)", dark: "hsl(243, 100%, 7%)" },
+  accent: {
+    light: "hsl(242, 100%, 62%)",
+    dark: "hsl(242, 100%, 38%)",
   },
 };
 
@@ -39,4 +44,11 @@ function useThemeColor(keys) {
   return colorMap[keys][colorMode];
 }
 
-export { colorMap, useThemeColors, useThemeColor };
+function useThemeConstantColor(keys, mode = "light") {
+  if (Array.isArray(keys)) {
+    return keys.map((key) => colorMap[key][mode]);
+  }
+  return colorMap[keys][mode];
+}
+
+export { colorMap, useThemeColors, useThemeColor, useThemeConstantColor };
