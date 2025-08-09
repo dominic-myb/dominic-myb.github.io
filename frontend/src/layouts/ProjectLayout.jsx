2fa1906da6c71b1ef5d872dcfb31e29628da3d8d
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2025-08-09 22:52:45 +0800

    [upd] removed vstack

--- frontend/src/layouts/ProjectLayout.jsx
+++ frontend/src/layouts/ProjectLayout.jsx
@@ -1,22 +1,16 @@
-import { Container, VStack, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import { useThemeColor } from "@/assets/colors";
 
 export default function ProjectLayout({ children }) {
-  const [cardBd, cardBg, titleFont] = useThemeColor(
-    ["cardBd",
-    "cardBg",
-    "titleFont"]
-  );
+  const [titleFont] = useThemeColor(["titleFont"]);
   return (
     <Container id="projects" maxW="container.lg" scrollMarginTop="75px">
-      <VStack bg={cardBg} border={`1px solid ${cardBd}`} borderRadius="xl">
-        <Heading mt={6} fontSize="3xl" color={titleFont}>
+        <Heading mb={6} mt={2} fontSize="3xl" color={titleFont} textAlign="center">
           Projects
         </Heading>
-        <SimpleGrid p={6} columns={2} gap={6}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} gap={6} my={6}>
           {children}
         </SimpleGrid>
-      </VStack>
     </Container>
   );
 }
