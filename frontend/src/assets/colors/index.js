8581cf8516b26f5bcc7ed2c96a88a30789042bab
Dominic Esguerra <desguerra246@gmail.com>
2025-08-13 22:53:23 +0800

    [upd] colors

--- frontend/src/assets/colors/index.js
+++ frontend/src/assets/colors/index.js
@@ -2,22 +2,22 @@ import { useColorMode } from "@chakra-ui/react";
 
 const colorMap = {
   logo: {
-    light: "linear-gradient(to-r, #00b09b, #96c93d)",
-    dark: "linear-gradient(to-r, #00b09b, #96c93d)",
+    light: "linear-gradient(to right, #3a7bd5, #3a6073)",
+    dark: "linear-gradient(to right, #3a7bd5, #3a6073)",
   },
   invertColor: { light: "hsl(0, 0%, 95%)", dark: "hsl(0, 0%, 5%)" },
   titleFont: { light: "hsl(246, 76%, 5%)", dark: "hsl(246, 76%, 95%)" },
   bodyFont: { light: "hsl(0, 0%, 20%)", dark: "hsl(0, 0%, 80%)" },
   smallFont: { light: "hsl(0, 0%, 40%)", dark: "hsl(0, 0%, 60%)" },
   bg: { light: "hsl(240, 100%, 100%)", dark: "	hsl(240, 60%, 1%)" },
-  cardBg: { light: "hsl(240, 16%, 94%)", dark: "hsla(0, 0%, 90%, 0.05)" },
+  cardBg: { light: "hsl(240, 58%, 96%)", dark: "hsla(0, 0%, 90%, 0.05)" },
   cardBd: {
     light: "hsla(0, 0%, 10%, 0.125)",
     dark: "hsla(0, 0%, 90%, 0.125)",
   },
   btnBg: { light: "hsla(0, 0%, 10%, 0.125)", dark: "hsla(0, 0%, 80%, 0.125)" },
   primary: { light: "hsl(243, 68%, 48%)", dark: "hsl(243, 68%, 52%)" },
-  secondary: { light: "hsl(243, 100%, 93%)", dark: "hsl(243, 100%, 7%)" },
+  secondary: { light: "hsl(243, 100%, 93%)", dark: "hsla(0, 0%, 100%, 0.08)" },
   accent: {
     light: "hsl(242, 100%, 62%)",
     dark: "hsl(242, 100%, 38%)",
