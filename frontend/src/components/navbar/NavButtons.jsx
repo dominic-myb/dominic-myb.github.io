23595a4b9abefe0434a01a8d4a06727a4a80e894
Dominic Esguerra <desguerra246@gmail.com>
2025-08-20 23:24:36 +0800

    [add] implement navigation buttons with color mode toggle

---
+++ frontend/src/components/navbar/NavButtons.jsx
@@ -0,0 +1,21 @@
+import { HStack, IconButton } from "@chakra-ui/react";
+import { SunIcon, MoonIcon, HamburgerIcon } from "@chakra-ui/icons";
+// change icons to react icons
+
+const NavButtons = ({ colorMode, onToggleColorMode, onOpenDrawer }) => (
+  <HStack>
+    <IconButton
+      onClick={onToggleColorMode}
+      icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+    />
+    <IconButton
+      display={{ base: "flex", md: "none" }}
+      icon={<HamburgerIcon />}
+      onClick={onOpenDrawer}
+      aria-label="Open menu"
+      variant="ghost"
+    />
+  </HStack>
+);
+
+export default NavButtons;
