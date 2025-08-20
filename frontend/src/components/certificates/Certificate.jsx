4297d6cede5cee28854ee695c10a79347f77ddde
Dominic Esguerra <desguerra246@gmail.com>
2025-08-20 23:25:39 +0800

    refactor: update Certificate component to use CSS modules for styling

--- frontend/src/components/certificates/Certificate.jsx
+++ frontend/src/components/certificates/Certificate.jsx
@@ -1,23 +1,22 @@
-import "./index.css";
 import { VStack, Heading, SimpleGrid } from "@chakra-ui/react";
 import { CertificateCard } from "./";
+import styles from "./Certificate.module.css";
 
-export default function Certificate({ cert }) {
-  return (
-    <VStack>
-      <Heading mt={6} fontSize="2xl" className="certificate-title">
-        Certificates
-      </Heading>
-      <SimpleGrid columns={{ base: 1, md: 3 }} p={6} gap={6}>
-        {cert.map(({ title, platform, link }, idx) => (
-          <CertificateCard
-            key={idx}
-            title={title}
-            platform={platform}
-            link={link}
-          />
-        ))}
-      </SimpleGrid>
-    </VStack>
-  );
-}
+const Certificate = ({ cert }) => (
+  <VStack>
+    <Heading mt={6} fontSize="2xl" className={styles.title}>
+      Certificates
+    </Heading>
+    <SimpleGrid columns={{ base: 1, md: 3 }} p={6} gap={6}>
+      {cert.map(({ title, platform, link }, idx) => (
+        <CertificateCard
+          key={idx}
+          title={title}
+          platform={platform}
+          link={link}
+        />
+      ))}
+    </SimpleGrid>
+  </VStack>
+);
+export default Certificate;
