d466a3a2858f7d2b263c950a5cef6c569570f235
Dominic Esguerra <desguerra246@gmail.com>
2025-08-20 23:26:49 +0800

    refactor: simplify Namecard component structure and improve styling integration

--- frontend/src/components/namecard/Namecard.jsx
+++ frontend/src/components/namecard/Namecard.jsx
@@ -1,37 +1,36 @@
 import { VStack, Stack } from "@chakra-ui/react";
 import NamecardImage from "./NamecardImage";
-import NamecardTextContent from "./NamecardTextContent";
 import NamecardButtons from "./NamecardButtons";
+import NamecardTextContent from "./NamecardTextContent";
+import styles from "./Namecard.module.css";
 
-export default function Namecard({ avatar, textContent, socialLinks }) {
-  return (
-    <VStack h="full" justifyContent="center" p={4}>
-      <Stack
-        className="no-select"
-        overflow="hidden"
-        alignItems="center"
-        gap={4}
-        direction={{
-          base: "column",
-          sm: "row",
-          md: "column",
-          lg: "row",
+const Namecard = ({ avatar, textContent, socialLinks }) => (
+  <VStack className={styles.container} p={4}>
+    <Stack
+      className={`${styles.wrapper} no-select`}
+      gap={4}
+      direction={{
+        base: "column",
+        sm: "row",
+        md: "column",
+        lg: "row",
+      }}
+    >
+      <NamecardImage avatar={avatar} />
+      <VStack
+        alignItems={{
+          base: "center",
+          sm: "flex-start",
+          md: "center",
+          lg: "flex-start",
         }}
+        gap={1}
       >
-        <NamecardImage avatar={avatar} />
-        <VStack
-          alignItems={{
-            base: "center",
-            sm: "flex-start",
-            md: "center",
-            lg: "flex-start",
-          }}
-          gap={1}
-        >
-          <NamecardTextContent textContent={textContent} />
-          <NamecardButtons socialLinks={socialLinks} />
-        </VStack>
-      </Stack>
-    </VStack>
-  );
-}
+        <NamecardTextContent textContent={textContent} />
+        <NamecardButtons socialLinks={socialLinks} />
+      </VStack>
+    </Stack>
+  </VStack>
+);
+
+export default Namecard;
