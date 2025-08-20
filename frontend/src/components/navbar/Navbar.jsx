e6945725f74b26e98b2763e3c9a5fd8c2894de18
Dominic Esguerra <desguerra246@gmail.com>
2025-08-20 23:25:04 +0800

    [add] implement Navbar component with responsive design and color mode toggle

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import {
+  Container,
+  HStack,
+  useDisclosure,
+  useColorMode,
+} from "@chakra-ui/react";
+import { useThemeColor } from "@/assets/colors";
+import NavLogo from "./NavLogo";
+import NavLinksContainer from "./NavLinksContainer";
+import NavbarDrawer from "./NavbarDrawer";
+import NavButtons from "./NavButtons";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Container
+      className={styles.navbar}
+      maxW="container.xxl"
+      bg={useThemeColor("bg")}
+    >
+      <Container maxW="container.lg" py={4}>
+        <HStack className={styles.navbar_wrapper}>
+          <NavLogo />
+          <HStack gap={6}>
+            <NavLinksContainer />
+            <NavButtons
+              colorMode={colorMode}
+              onToggleColorMode={toggleColorMode}
+              onOpenDrawer={onOpen}
+            />
+          </HStack>
+        </HStack>
+      </Container>
+      <NavbarDrawer isOpen={isOpen} onClose={onClose} />
+    </Container>
+  );
+};
+
+export default Navbar;
