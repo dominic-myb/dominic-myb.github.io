cdb4e278b8e0451f27fd9ed1c0c50c6373049032
Dominic Esguerra <desguerra246@gmail.com>
2025-08-21 23:13:19 +0800

    refactor: simplify App component structure and remove unused imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "@/assets/styles/global.css";
-import "@/assets/styles/variables.css";
 import "@/assets/styles/responsive.css";
-import "@/assets/styles/animations.css";
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path={"/"} element={<Home />}></Route>
-    </Routes>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route path={"/"} element={<Home />}></Route>
+  </Routes>
+);
+
+export default App;
