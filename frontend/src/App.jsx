92065e3b25e83e9b9e59cb3b1303141c45eba3e3
Dominic Esguerra <desguerra246@gmail.com>
2025-08-22 01:35:07 +0800

    refactor: move responsive styles from App to Timeline module for better organization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "@/assets/styles/global.css";
-import "@/assets/styles/responsive.css";
 
 const App = () => (
   <Routes>
