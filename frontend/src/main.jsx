b242207a1767efb4e8ef0b2641cccd11305acac7
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:10:21 +0800

    [rem] theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import theme from "@/theme/theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
