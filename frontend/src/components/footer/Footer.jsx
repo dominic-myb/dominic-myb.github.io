5ab2f93a4875aeed314169e071383f6107f8b247
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:18:45 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,23 +1,14 @@
 import { VStack, Text } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  const [bg, bodyFont, smallFont] = useThemeColor([
-    "bg",
-    "bodyFont",
-    "smallFont",
-  ]);
-  
   return (
-    <VStack as="footer" bg={bg} className={styles.footer}>
-      <Text className={styles.copyright} color={bodyFont}>
+    <VStack as="footer" className={styles.footer}>
+      <Text className={styles.copyright}>
         © 2024 - {new Date().getFullYear()} Dominic Esguerra. All rights
         reserved.
       </Text>
-      <Text className={styles.credit_line} color={smallFont}>
-        Made with React + ChakraUI
-      </Text>
+      <Text className={styles.credit_line}>Made with React + ChakraUI</Text>
     </VStack>
   );
 };
