7dbea2f879ac9b2995974b98cecbcf039d13e589
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:21:11 +0800

    refactor: move styling to css

--- frontend/src/components/navbar/NavLinksContainer.jsx
+++ frontend/src/components/navbar/NavLinksContainer.jsx
@@ -1,11 +1,11 @@
 import { HStack } from "@chakra-ui/react";
 import NavLink from "./NavLink";
+import styles from "./Navbar.module.css";
 
 const NavLinksContainer = () => (
   <HStack
     display={{ base: "none", md: "flex" }}
-    justifyContent="flex-end"
-    gap={6}
+    className={styles.navlinks_container}
   >
     {[{ name: "about" }, { name: "projects" }, { name: "contact" }].map(
       (link, idx) => (
