7b46a60dba294346dc4ea1cad3cfae27b5bbb0e9
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:17:45 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/contacts/ContactContainer.jsx
+++ frontend/src/components/contacts/ContactContainer.jsx
@@ -1,12 +1,9 @@
 import { Container, VStack } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Contacts.module.css";
 
 const ContactContainer = ({ children }) => (
   <Container as="section" id="contact" className={styles.contact_container}>
-    <VStack bg={useThemeColor("cardBg")} className={styles.contact_wrapper}>
-      {children}
-    </VStack>
+    <VStack className={styles.contact_wrapper}>{children}</VStack>
   </Container>
 );
 
