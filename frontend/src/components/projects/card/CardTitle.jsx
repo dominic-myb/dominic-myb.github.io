9f1f12fac8597731ad1104d7a8845c909bb40b4c
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:21:44 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/projects/card/CardTitle.jsx
+++ frontend/src/components/projects/card/CardTitle.jsx
@@ -1,11 +1,10 @@
 import { HStack, Link } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Card.module.css";
 
 const CardTitle = ({ title }) => (
   <HStack>
-    <Link className={styles.card_title} color={useThemeColor("titleFont")}>
+    <Link className={styles.card_title}>
       {title}
       <ExternalLinkIcon className={styles.card_title_icon} boxSize={4} />
     </Link>
