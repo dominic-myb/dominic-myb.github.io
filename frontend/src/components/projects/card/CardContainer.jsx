78402004e35b47ef09b5b5c7c6f319c1e1e06400
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:21:34 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/projects/card/CardContainer.jsx
+++ frontend/src/components/projects/card/CardContainer.jsx
@@ -1,15 +1,8 @@
 import { VStack } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Card.module.css";
 
 const CardContainer = ({ children }) => (
-  <VStack
-    className={styles.card_container}
-    border={`1px solid ${useThemeColor("cardBd")}`}
-    // bg={useThemeColor("cardBg")}
-  >
-    {children}
-  </VStack>
+  <VStack className={styles.card_container}>{children}</VStack>
 );
 
 export default CardContainer;
