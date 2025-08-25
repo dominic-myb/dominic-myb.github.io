d765a7b318a4f09eb0f4d98f2da974248e977b07
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:21:18 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/navbar/NavLogo.jsx
+++ frontend/src/components/navbar/NavLogo.jsx
@@ -1,12 +1,10 @@
 import { Heading, useBreakpointValue } from "@chakra-ui/react";
-import { useThemeConstantColor } from "@/assets/colors";
 import styles from "./Navbar.module.css";
 
 const NavLogo = ({ ...props }) => (
   <Heading
     className={`${styles.logo} no-select`}
     fontSize={useBreakpointValue({ base: 24, md: 28 })}
-    bgGradient={useThemeConstantColor("logo", "light")}
     {...props}
   >
     _dominic
