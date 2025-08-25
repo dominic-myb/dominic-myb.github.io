1607b3cdcabacd73986a4341ba37a44a3285e821
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:17:33 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/certificates/CertificateCard.jsx
+++ frontend/src/components/certificates/CertificateCard.jsx
@@ -1,9 +1,7 @@
 import { Text, Button, VStack } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Certificate.module.css";
 
 const CertificateCard = ({ title, platform, link }) => {
-  const [titleFont, smallFont] = useThemeColor(["titleFont", "smallFont"]);
   return (
     <Button
       className={styles.card_button}
@@ -12,13 +10,9 @@ const CertificateCard = ({ title, platform, link }) => {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <VStack className={styles.card_wrapper}> 
-        <Text className={styles.card_title} color={titleFont}>
-          {title}
-        </Text>
-        <Text className={styles.card_platform} color={smallFont}>
-          {platform}
-        </Text>
+      <VStack className={styles.card_wrapper}>
+        <Text className={styles.card_title}>{title}</Text>
+        <Text className={styles.card_platform}>{platform}</Text>
       </VStack>
     </Button>
   );
