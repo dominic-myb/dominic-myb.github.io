be9ba8dc5db701a189597222a6b76564f6c2dd65
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:17:39 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/contacts/ContactBody.jsx
+++ frontend/src/components/contacts/ContactBody.jsx
@@ -1,13 +1,8 @@
 import { Box } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Contacts.module.css";
 
 const ContactBody = ({ children }) => (
-  <Box
-    w={{ base: "70%", md: "60%" }}
-    className={styles.contact_body}
-    border={`1px solid ${useThemeColor("cardBd")}`}
-  >
+  <Box w={{ base: "70%", md: "60%" }} className={styles.contact_body}>
     {children}
   </Box>
 );
