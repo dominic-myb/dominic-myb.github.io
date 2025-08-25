98077bcaa1e5f1ad5d9291e42578ac74d9e68b0e
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:21:27 +0800

    refactor: remove all colors from js and move to css

--- frontend/src/components/projects/ProjectsContainer.jsx
+++ frontend/src/components/projects/ProjectsContainer.jsx
@@ -1,10 +1,9 @@
 import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
-import { useThemeColor } from "@/assets/colors";
 import styles from "./Projects.module.css";
 
 const ProjectsContainer = ({ children }) => (
   <Container as="section" id="projects" className={styles.project_container}>
-    <Heading className={styles.heading} color={useThemeColor("titleFont")}>
+    <Heading className={styles.heading}>
       Projects
     </Heading>
     <SimpleGrid columns={{ base: 1, md: 2 }} className={styles.grid}>
