0d223a337f7e08d46521ffcaaf6ed3ed0ada4487
Dominic Esguerra <desguerra246@gmail.com>
2025-08-25 10:23:23 +0800

    refactor: code formatiing

--- frontend/src/sections/ProjectSection.jsx
+++ frontend/src/sections/ProjectSection.jsx
@@ -2,17 +2,10 @@ import { ProjectsContainer, Card } from "@/components/projects";
 import projectData from "@/assets/data/projects-test.json";
 
 const ProjectSection = () => {
-  console.log(projectData);
   return (
     <ProjectsContainer>
       {projectData.map(({ image, title, desc, tools }, idx) => (
-        <Card
-          key={idx}
-          image={image}
-          title={title}
-          desc={desc}
-          tools={tools}
-        />
+        <Card key={idx} image={image} title={title} desc={desc} tools={tools} />
       ))}
     </ProjectsContainer>
   );
