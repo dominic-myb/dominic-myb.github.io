1007e2ce4f9b3ae850ddb5c2ded5c7c82e9c7887
Dominic Esguerra <desguerra246@gmail.com>
2025-08-26 23:38:17 +0800

    [upd] add vstack as wrapper

--- frontend/src/components/projects/ProjectsContainer.jsx
+++ frontend/src/components/projects/ProjectsContainer.jsx
@@ -1,14 +1,14 @@
-import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Container, VStack, Heading, SimpleGrid } from "@chakra-ui/react";
 import styles from "./Projects.module.css";
 
 const ProjectsContainer = ({ children }) => (
   <Container as="section" id="projects" className={styles.project_container}>
-    <Heading className={styles.heading}>
-      Projects
-    </Heading>
-    <SimpleGrid columns={{ base: 1, md: 2 }} className={styles.grid}>
-      {children}
-    </SimpleGrid>
+    <VStack className={styles.wrapper}>
+      <Heading className={styles.heading}>🚀 Projects</Heading>
+      <SimpleGrid columns={{ base: 1, md: 2 }} className={styles.grid}>
+        {children}
+      </SimpleGrid>
+    </VStack>
   </Container>
 );
 
