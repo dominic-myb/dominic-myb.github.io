de759c403de857db8076cc89e8d74c9d62174ec4
Dominic Esguerra <desguerra246@gmail.com>
2025-08-26 23:37:32 +0800

    [upd] add scroll spy navigation

--- frontend/src/components/navbar/NavLink.jsx
+++ frontend/src/components/navbar/NavLink.jsx
@@ -1,11 +1,11 @@
 import { Link } from "@chakra-ui/react";
 import styles from "./Navbar.module.css";
 
-const NavLink = ({ linkName }) => {
+const NavLink = ({ linkName, isActive }) => {
   return (
     <Link
-      className={`${styles.navlink} no-select`}
-      href={`#${linkName.toLowerCase()}`}
+      className={`${styles.navlink} no-select ${isActive ? styles.active : ""}`}
+      href={`#${linkName}`}
       fontSize={{ base: 16, lg: 18 }}
     >
       {linkName}
