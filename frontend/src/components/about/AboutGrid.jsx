da41a0c5f9bdb106da28871b574e8fdbae5f9e17
Dominic Esguerra <desguerra246@gmail.com>
2025-08-26 23:31:11 +0800

    [upd] grid size

--- frontend/src/components/about/AboutGrid.jsx
+++ frontend/src/components/about/AboutGrid.jsx
@@ -1,20 +1,8 @@
-import { Grid, useBreakpointValue } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 
 const AboutGrid = ({ children }) => {
-  const rows =
-    useBreakpointValue({
-      base: "repeat(6, 1fr)",
-      md: "repeat(7, 1fr)",
-      lg: "repeat(4, 1fr)",
-    }) ?? "repeat(5, 1fr)";
-  const cols =
-    useBreakpointValue({
-      base: "repeat(4, 1fr)",
-      md: "repeat(4, 1fr)",
-      lg: "repeat(3, 1fr)",
-    }) ?? "repeat(4, 1fr)";
   return (
-    <Grid templateColumns={cols} templateRows={rows} gap={4}>
+    <Grid templateColumns="repeat(auto-fit, auto)" width="full" gap={4}>
       {children}
     </Grid>
   );
