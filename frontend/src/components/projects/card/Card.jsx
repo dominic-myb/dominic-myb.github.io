7d0082683e1d05f225f09e61dbee50499afb28de
Dominic Esguerra <desguerra246@gmail.com>
2025-08-31 07:44:19 +0800

    [upd] add playing video on hover

--- frontend/src/components/projects/card/Card.jsx
+++ frontend/src/components/projects/card/Card.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { VStack } from "@chakra-ui/react";
 import CardContainer from "./CardContainer";
 import CardImageWrapper from "./CardImageWrapper";
@@ -5,19 +6,46 @@ import CardImage from "./CardImage";
 import CardTitle from "./CardTitle";
 import CardDesc from "./CardDesc";
 import CardTools from "./CardTools";
+import CardVideoPreview from "./CardVideoPreview";
 import styles from "./Card.module.css";
 
-const Card = ({ image, title, desc, tools }) => (
-  <CardContainer>
-    <CardImageWrapper>
-      <CardImage image={image} alt={title} />
-    </CardImageWrapper>
-    <VStack className={styles.card_wrapper}>
-      <CardTitle title={title} />
-      <CardDesc desc={desc} />
-      <CardTools tools={tools} />
-    </VStack>
-  </CardContainer>
-);
+const Card = ({ image, title, desc, src, tools }) => {
+  const videoRef = useRef(null);
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 0;
+      const playPromise = videoRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.catch((err) => {
+          if (err.name !== "AbortError") {
+            console.error("Video play failed:", err);
+          }
+        });
+      }
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current && !videoRef.current.paused) {
+      videoRef.current.pause();
+    }
+  };
+  return (
+    <CardContainer>
+      <CardImageWrapper
+        handleMouseEnter={handleMouseEnter}
+        handleMouseLeave={handleMouseLeave}
+      >
+        <CardImage image={image} alt={title} />
+        <CardVideoPreview src={src} ref={videoRef} />
+      </CardImageWrapper>
+      <VStack className={styles.card_wrapper}>
+        <CardTitle title={title} />
+        <CardDesc desc={desc} />
+        <CardTools tools={tools} />
+      </VStack>
+    </CardContainer>
+  );
+};
 
 export default Card;
