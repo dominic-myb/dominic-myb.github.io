d432ea3a5715b63c13712397d2f5da6a73a21c83
Dominic Esguerra <desguerra246@gmail.com>
2025-08-31 07:45:28 +0800

    [upd] add two source second one is fallback

--- frontend/src/components/projects/card/CardVideoPreview.jsx
+++ frontend/src/components/projects/card/CardVideoPreview.jsx
@@ -2,18 +2,20 @@ import { forwardRef } from "react";
 import { Box } from "@chakra-ui/react";
 import styles from "./Card.module.css";
 
-const CardVideoPreview = forwardRef(({ src, ...props }, ref) => {
+const CardVideoPreview = forwardRef(({ src }, ref) => {
   return (
     <Box
       as="video"
       ref={ref}
-      src={src}
       muted
       loop
+      playsInline
       preload="metadata"
       className={styles.card_video}
-      {...props}
-    />
+    >
+      <source src={`/assets/projects/${src}.webm`} type="video/webm" />
+      <source src={`/assets/projects/${src}.mp4`} type="video/mp4" />
+    </Box>
   );
 });
 
