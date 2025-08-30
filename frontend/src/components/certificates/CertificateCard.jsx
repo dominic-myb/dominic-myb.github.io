986ef82c19c222f85a7ee3fa93c773a28f5d9203
Dominic Esguerra <desguerra246@gmail.com>
2025-08-30 17:21:09 +0800

    [upd] card desc remove image

--- frontend/src/components/certificates/CertificateCard.jsx
+++ frontend/src/components/certificates/CertificateCard.jsx
@@ -1,25 +1,31 @@
-import { Text, Button, VStack, Image, HStack } from "@chakra-ui/react";
+import { Text, Button, VStack, Image, HStack, Box } from "@chakra-ui/react";
 import styles from "./Certificate.module.css";
 
 const CertificateCard = ({ title, platform, link }) => (
-  <Button
+  <Box
     className={styles.card_button}
     as="a"
     href={link}
     target="_blank"
     rel="noopener noreferrer"
   >
-    <HStack>
-      <Image
+    <HStack align="start">
+      {/* <Image
         className={styles.card_icon}
         src={`/assets/icons/${platform.toLowerCase()}.svg`}
         boxSize="50px"
-      />
+      /> */}
       <VStack className={styles.card_wrapper}>
-        <Text className={styles.card_title}>{title}</Text>
-        <Text className={styles.card_platform}>{platform}</Text>
+        <Box>
+          <Text className={styles.card_title}>{title}</Text>
+          <Text className={styles.card_platform}>{platform}</Text>
+        </Box>
+        <Text className={styles.card_desc}>
+          Demonstrated proficiency in HTML, CSS, and modern design principles
+          including accessibility, mobile-first design, and responsive layouts.
+        </Text>
       </VStack>
     </HStack>
-  </Button>
+  </Box>
 );
 export default CertificateCard;
