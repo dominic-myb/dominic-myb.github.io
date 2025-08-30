755d65a0ffe975e5d82dfd97e6a5d0153d46e6a0
Dominic Esguerra <desguerra246@gmail.com>
2025-08-30 17:21:30 +0800

    [upd] remove techstack icon

--- frontend/src/components/techstacks/Techstacks.jsx
+++ frontend/src/components/techstacks/Techstacks.jsx
@@ -1,57 +1,55 @@
-import { VStack, Heading, GridItem, Flex } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
-import TechstackIcons from "./TechstackIcons";
+import { VStack, Heading, GridItem, Flex, Box, Text } from "@chakra-ui/react";
 import styles from "./Techstacks.module.css";
 
 const Techstacks = () => {
-  const [isAnimated, setIsAnimated] = useState(true);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIsAnimated((prev) => !prev);
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const toolNames = [
-    "html5",
-    "css3",
-    "javascript",
-    "react",
-    "nodejs",
-    "mongodb",
-    "mysql",
-    "php",
-    "cpp",
-    "jquery",
-    "figma",
-    "godot",
-    "bootstrap",
-  ];
-
+  const data = {
+    "Programming Languages": [
+      "JavaScript/TypeScript",
+      "Python",
+      "C++",
+      "GDScript",
+    ],
+    Frontend: ["React.js", "TailwindCSS", "Bootstrap", "ChakraUI", "HTML/CSS"],
+    Backend: ["Node.js","Express.js", "MongoDB", "MySQL"],
+    Tools: ["Git", "Github", "Postman", "Godot", "VSCode"],
+  };
   return (
     <GridItem
       colSpan={{ base: 4, md: 2, lg: 2 }}
       className={styles.main_container}
     >
       <VStack className={styles.techstacks_container}>
-        <Heading className={`${styles.techstacks_heading}`}>🛠️Techstacks</Heading>
-        <Flex className={styles.techstacks_flex}>
-          {toolNames.map((img, idx) => (
-            <TechstackIcons
-              key={img}
-              flexClass={`${styles.techstack} ${
-                isAnimated ? styles.moveup : ""
-              }`}
-              imgPath={`/assets/icons/${img}.svg`}
-              imgName={img}
-              imgAlt={`${img}.svg`}
-              imgIdx={idx}
-            />
+        <Heading className={`${styles.techstacks_heading}`}>
+          🛠️Technical Skills
+        </Heading>
+        <Box>
+          {Object.entries(data).map(([category, items]) => (
+            <Box key={category} mb={2}>
+              <Text fontSize="md" fontWeight={800}>
+                {category}:&nbsp;
+              </Text>
+              {items.map((item, idx) => (
+                <Box
+                  as="span"
+                  key={idx}
+                  className={styles.item}
+                  mr={idx < items.length - 1 ? 1 : 0}
+                >
+                  {item}
+                </Box>
+              ))}
+            </Box>
           ))}
-        </Flex>
+        </Box>
       </VStack>
     </GridItem>
   );
 };
+
+const Item = (item) => (
+  <Box as="span" border="1px solid white">
+    {item}
+  </Box>
+);
 
 export default Techstacks;
