00820e420f0abdf67390bed91b6d57bf8a0812ca
Dominic Esguerra <desguerra246@gmail.com>
2025-08-30 17:20:38 +0800

    [upd] about grid

--- frontend/src/components/about/AboutGrid.jsx
+++ frontend/src/components/about/AboutGrid.jsx
@@ -2,7 +2,7 @@ import { Grid } from "@chakra-ui/react";
 
 const AboutGrid = ({ children }) => {
   return (
-    <Grid templateColumns="repeat(auto-fit, auto)" width="full" gap={4}>
+    <Grid templateColumns="repeat(auto-fit, auto)" gap={4}>
       {children}
     </Grid>
   );
