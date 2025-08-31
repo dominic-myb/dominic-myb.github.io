36a5c5084f65ba01608a78fff8aa66412a0877c9
Dominic Esguerra <desguerra246@gmail.com>
2025-08-31 08:31:24 +0800

    [upd] configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "./",
   build: {
     outDir: "dist",
     assetsDir: "assets",
