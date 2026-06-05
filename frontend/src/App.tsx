029ffa5a06ed709b38dc7e0a6dfbd1c8556373fb
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2026-06-05 11:37:51 +0800

    feat: enhance App component structure by adding Header and Hero sections for improved layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { Header } from "@/components/ui/Header";
+import { Hero } from "@/components/sections/Hero";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
 }
 
 export default App;
