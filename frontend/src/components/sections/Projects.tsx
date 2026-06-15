a3b3c041436d123800d2a5205fa5b6adc798d884
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2026-06-15 13:55:01 +0800

    feat: update Projects component to include styling and header for improved presentation

--- frontend/src/components/sections/Projects.tsx
+++ frontend/src/components/sections/Projects.tsx
@@ -1,5 +1,11 @@
 function Projects() {
-  return <section>Projects</section>;
+  return (
+    <section className="w-100dvw h-screen">
+      <div className="py-2">
+        <h2 className="text-black/80">//Projects</h2>
+      </div>
+    </section>
+  );
 }
 
 export { Projects };
