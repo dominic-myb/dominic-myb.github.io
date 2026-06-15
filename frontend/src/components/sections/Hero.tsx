b3472e9ef899d2f07e0148333e6c862927452815
Dominic Esguerra <dominic.arenal.esguerra@gmail.com>
2026-06-15 13:54:55 +0800

    feat: enhance Hero component with new layout and content for improved user engagement

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -1,5 +1,15 @@
 function Hero() {
-  return <section className="w-100dvw h-screen"></section>;
+  return (
+    <section id="hero" className="bg-linear-to-b from-green-200 via-emerald-400 to-primary">
+      <div className="h-screen w-full flex flex-col justify-center items-center text-center gap-4">
+        <h2 className="text-2xl italic tracking-wider">Hello World!</h2>
+        <h1 className="text-7xl font-bold tracking-tight text-neutral-dark">
+          Dominic Esguerra
+        </h1>
+        <p className="text-2xl tracking-wide">Junior Software Developer</p>
+      </div>
+    </section>
+  );
 }
 
 export { Hero };
